844a256dc8161d019528eded1ec6238164593b5e
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-06 09:51:48 -0800

    added notes in js file for building out logic

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,11 +1,34 @@
 'use strict';
 
-// used youtube to figure out modal js https://www.youtube.com/watch?v=KjQ8uvAt9kQ
+// global variables
 let tileSelect = document.querySelector('#gameboard');
 let modalBg = document.querySelector('.modal-bg');
+let playerScore = 0;
+let gameCategories = [];
 
+// Category Constructor
+
+
+
+// add questions method
+
+
+
+
+
+
+// validate answer function
+
+
+
+
+
+
+// used youtube to figure out modal js https://www.youtube.com/watch?v=KjQ8uvAt9kQ
 function questionClick(event) { //eslint-disable-line
   modalBg.classList.add('bg-active');
 }
+
+// Submit eventlistener
 
 tileSelect.addEventListener('click', questionClick);
