7917164768b3e2b3415e758db10e280149d4dd92
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-06 10:20:38 -0800

    built category constructor and started with our new category instances

--- js/gameboard.js
+++ js/gameboard.js
@@ -7,15 +7,45 @@ let playerScore = 0;
 let gameCategories = [];
 
 // Category Constructor
-
+function Category(catName) {
+  this.catName = catName;
+  this.catQuestions = [];
+  gameCategories.push(this);
+}
 
 
 // add questions method
+Category.prototype.addQuestion = function(question, answer, a, b, c, points){
+  this.catQuestions.push({
+    question,
+    answer,
+    a,
+    b,
+    c,
+    points,
+  });
+};
 
 
+// new category instances
+let htmlCategory = new Category('HTML');
+htmlCategory.addQuestion('Test Question', 'banana', 'wrong', 'correct', 'banana',100);
+htmlCategory.addQuestion('Test Q2', 'right', 'right', 'wrong', 'yeah', 100);
 
 
+let cssCategory = new Category('CSS');
+cssCategory.addQuestion('Test Q1', 'correctanswer', 'optionA', 'correctanswer', 'optionC', 100);
 
+let jsCategory = new Category('JAVASCRIPT');
+jsCategory.addQuestion();
+
+
+let gitCategory = new Category('GIT COMMANDS');
+gitCategory.addQuestion();
+
+
+let ghCategory = new Category('GITHUB');
+ghCategory.addQuestion();
 
 // validate answer function
 
