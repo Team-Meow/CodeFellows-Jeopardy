b27acf7405974ff13fdab3f9143569ec1b812475
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-05 22:28:13 -0800

    added javascript for modal rendering

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// used youtube to figure out modal js https://www.youtube.com/watch?v=KjQ8uvAt9kQ
+let tileSelect = document.querySelector('#gameboard');
+let modalBg = document.querySelector('.modal-bg');
+
+function questionClick(event) { //eslint-disable-line
+  modalBg.classList.add('bg-active');
+}
+
+tileSelect.addEventListener('click', questionClick);
